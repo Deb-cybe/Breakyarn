e7822a004bf93230047e60b781b914f38cfae4ed
Deb-cybe <dmjee1331@gmail.com>
2023-04-23 11:41:48 +0530

    Added Database

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+const {db}=require('./util/admin');
 const express=require('express');
 const cors=require('cors');
-
-const app=express();
+const mongoose=require('mongoose');
 const port=process.env.PORT || 60118;
-app.use(cors());
+
+const NamesModel=require('./models/Names');
+const app=express(); 
+app.use(express.json());
+app.use(cors()); 
+
+const {usernames}=require('./models/firenames');
 
 app.get('/',(req,res)=>{
-    res.send("This works.");
-})
+    // res.json({ message: "Hello from server!" });
+    res.send("Hello from the server");
+});
 
-app.listen(port,()=>{
-    console.log(`Connected to port ${port}`);
-})
+//  firestore
+app.get('/names',usernames);
+const addNames = async (req, res) => {
+    const { name} = req.body
+    try {
+      const entry = db.collection('users').doc()
+      const entryObject = { 
+        name
+      }
+  
+      entry.set(entryObject);
+      res.status(200).send({
+        status: 'success',
+        message: 'entry added successfully',
+        data: entryObject
+      })
+    } catch(error) {
+        res.status(500).json(error.message)
+    }
+  }
+app.post('/setnames', addNames);
+//  firestore
+
+app.listen(port, console.log("Server running"));
